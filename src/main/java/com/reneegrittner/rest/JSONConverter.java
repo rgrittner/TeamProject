895cb60a50f99816da06f7c9371fb3741702c28b
xinanwan <xwan@madisoncollege.edu>
2018-03-25 16:02:24 -0500

    added composer dao

--- src/main/java/com/reneegrittner/rest/JSONConverter.java
+++ src/main/java/com/reneegrittner/rest/JSONConverter.java
@@ -1,9 +1,13 @@
 package com.reneegrittner.rest;
 
+import com.reneegrittner.entity.Composer;
+import com.reneegrittner.persistence.GenericDao;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 
 @Path("/JSONConverter")
@@ -19,6 +23,14 @@ public class JSONConverter {
         // Return a simple message
         String output = "Hello Everyone!";
         return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces("text/JSON")
+    public Response getComposers() {
+        GenericDao<Composer> dao = new GenericDao<>(Composer.class);
+        List<Composer> composers = dao.getAll();
+        return Response.status(200).entity(composers).build();
     }
 
 }
