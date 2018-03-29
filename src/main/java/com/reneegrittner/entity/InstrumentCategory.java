34f325bca8f6c16495a79381f234e661a44f4c3d
Renee Grittner <renee.grittner@gmail.com>
2018-03-28 20:05:57 -0500

    InstumentCategory is now not killing our service. It's good to go

--- src/main/java/com/reneegrittner/entity/InstrumentCategory.java
+++ src/main/java/com/reneegrittner/entity/InstrumentCategory.java
@@ -13,85 +13,85 @@ import java.util.Set;
  * categories will allow for better display and sorting.
  * @author Renee Grittner
  */
-//@Entity(name = "InstrumentCategory")
-//@Table(name = "Instrument_Category")
+@Entity(name = "InstrumentCategory")
+@Table(name = "Instrument_Category")
 public class InstrumentCategory {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-//    @GenericGenerator(name = "native", strategy = "native")
-//    private int id;
-//
-//    @Column(name = "category")
-//    private String category;
-//
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "category")
+    private String category;
+
 //    @OneToMany(mappedBy = "instrumentCategory", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
 //
 //    private Set<Instrument> instruments = new HashSet<>();
-//
-//    /**
-//     * Instantiates a new Instrument category.
-//     */
-//    public InstrumentCategory() {
-//    }
-//
-//    /**
-//     * Instantiates a new Instrument category.
-//     *
-//     * @param category the category
-//     */
-//    public InstrumentCategory(String category) {
-//
-//        this.category = category;
-//    }
-//
-//    /**
-//     * Gets id.
-//     *
-//     * @return the id
-//     */
-//    public int getId() {
-//        return id;
-//    }
-//
-//    /**
-//     * Sets id.
-//     *
-//     * @param id the id
-//     */
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    /**
-//     * Gets category.
-//     *
-//     * @return the category
-//     */
-//    public String getCategory() {
-//        return category;
-//    }
-//
-//    /**
-//     * Sets category.
-//     *
-//     * @param category the category
-//     */
-//    public void setCategory(String category) {
-//        this.category = category;
-//    }
-//
-//
+
+    /**
+     * Instantiates a new Instrument category.
+     */
+    public InstrumentCategory() {
+    }
+
+    /**
+     * Instantiates a new Instrument category.
+     *
+     * @param category the category
+     */
+    public InstrumentCategory(String category) {
+
+        this.category = category;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets category.
+     *
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Sets category.
+     *
+     * @param category the category
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
 //    /**
 //     * Add Instrument.
 //     *
-//     * @param instrument the composer
+//     * @param instrument the instrument
 //     */
 //    public void addInstrument(Instrument instrument) {
 //        instruments.add(instrument);
 //        instrument.setInstrumentCategory(this);
 //    }
-//
+
 //    /**
 //     * Remove instrument.
 //     *
@@ -101,26 +101,26 @@ public class InstrumentCategory {
 //        instruments.remove( instrument );
 //        instrument.setInstrumentCategory( null );
 //    }
-//
-//
-//
-//    @Override
-//    public String toString(){
-//        return "Instrument Category {id= " + id + " category= " + category;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        InstrumentCategory that = (InstrumentCategory) o;
-//        return id == that.id &&
-//                Objects.equals(category, that.category);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//
-//        return Objects.hash(id, category);
-//    }
+
+
+
+    @Override
+    public String toString(){
+        return "Instrument Category {id= " + id + " category= " + category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InstrumentCategory that = (InstrumentCategory) o;
+        return id == that.id &&
+                Objects.equals(category, that.category);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, category);
+    }
 }
